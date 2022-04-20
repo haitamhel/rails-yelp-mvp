160b711d3ea80798831d67b72385746bdf3aa39e
HAITAM EL OUANNAS <haitamelouannas@gmail.com>
2022-04-20 09:29:28 +0200

    exo 2 not finished yet

---
+++ app/controllers/restaurant_controller.rb
@@ -0,0 +1,20 @@
+class RestaurantController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+  def new
+    @restaurant = Restaurant.new
+  end
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to restaurant_path(@restaurant)
+  end
+  def show
+  end
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get    "restaurants",          to: "restaurant#index"
+
+  get    "restaurants/new",      to: "restaurant#new",  as: :new_restaurant
+  post   "restaurants",          to: "restaurant#create"
+
+  # NB: The `show` route needs to be *after* `new` route.
+  get    "restaurants/:id",      to: "restaurant#show", as: :restaurant
+
+  get    "restaurants/:id/reviews/new", to: "reviews#new",  as: :new_review
+  post   "restaurants/:id/reviews", to: "reviews#create"
 end
